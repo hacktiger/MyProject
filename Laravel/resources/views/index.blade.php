5e183b4e1a1a751693f29656e7eed178ced64cc2
unknown <hacktiegr1989@gmail.com>
2018-04-16 18:43:18 +0700

    index layout change for the better

--- Laravel/resources/views/index.blade.php
+++ Laravel/resources/views/index.blade.php
@@ -18,15 +18,12 @@ a:hover{
   cursor: pointer;
 }
 
-
-
-
 </style>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 @endsection
 @section('scripts')
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 @endsection
 
 @section('content')
@@ -45,19 +42,17 @@ a:hover{
       <div class="item active">
         <img src="https://i.imgur.com/qQIbhnJ.png" style="width:100%;">
         <div class="carousel-caption">
-          <h2><font color=black> Developer Sale: Square Enix</font></h3>
-          </div>
-        </div>
-
-        <div class="item">
-          <img src="https://i.imgur.com/5keALkN.png" style="width:100%;">
-        </div>
-        
-        <div class="item">
-          <img src="https://i.imgur.com/cz8Cxmr.png" style="width:100%;">
+          <h2><font color=black> Developer Sale: Square Enix</font></h2>
         </div>
       </div>
+      <div class="item">
+        <img src="https://i.imgur.com/5keALkN.png" style="width:100%;">
+      </div>
+      <div class="item">
+        <img src="https://i.imgur.com/cz8Cxmr.png" style="width:100%;">
+      </div>
 
+    </div>
       <!-- Left and right controls -->
       <a class="left carousel-control" href="#myCarousel" data-slide="prev">
         <span class="glyphicon glyphicon-chevron-left"></span>
@@ -66,42 +61,46 @@ a:hover{
       <a class="right carousel-control" href="#myCarousel" data-slide="next">
         <span class="glyphicon glyphicon-chevron-right"></span>
         <span class="sr-only">Next</span>
-      </a>
-    </div>
+      </a> 
   </div>
-  <br>
-
-  
-  <!-- prints out every game -->
-<div class="container">
-  <h3>All Games</h3>
-  <table class="table border">
-    <tbody>
-      <tr>
-        <td>Thumbnail</td>
-        <td>Title</td>
-        <td>Rating</td>
-        <td>Developer</td>  
-      </tr>
-      <h2></h2>
-      @foreach($game as $games)
-      <tr  class="clickable-row hover-row" data-href="/games/{{$games->title}}">
-        <td><img style="width:180px;height: 60px" src="/storage/cover_images/{{$games->image}}"></td>
-        <td><a href="/games/{{$games->title}}">{{$games->title}}</a></td>
-        <td>{{$games->avg_rating}} &ensp;<span class="fa fa-star" style="color:orange;"></span></td>
-        <td>{{$games->upload_by}}</td>
-      </tr>
-      @endforeach
-    </tbody>
-  </table>
 </div>
- @endsection
+<br>
+
+<!-- prints out every game -->
+<div class="container content-row">
+  <div class="row">
+    @foreach($game as $games)
+      <div class="col-lg-4 col-md-6 mb-4" style="margin-top:30px;">
+        <div class="card h-100" style="border-radius: 10px; border: 1px solid #ddd">
+          <!-- IMG -->
+          <a href="/games/{{$games->title}}">
+            <div style="">
+              <img style="border-top-left-radius: 10px; border-top-right-radius: 10px; border: 1px solid #ddd; max-height: 140px" src="/storage/cover_images/{{$games->image}}" class="card-img-top" alt="{{$games->title}}">
+            </div>
+          </a>
+          <!-- Body -->
+          <div class="card-body">
+            <h4 class="card-title"><a href="/games/{{$games->title}}">{{$games->title}}</a></h4>
+            <h5>Price : {{$games->price}}$</h5>
+            <p class="card-text">something about this game that takes really long sentences</p>
+          </div>
+          <!-- Footer -->
+          <div class="card-footer">
+            <i class="fa fa-star pull-right" style="color: orange; padding-top: 2px;"></i><small class="pull-right">{{$games->avg_rating}}</small>
+            <small >{{$games->upload_by}}</small>
+          </div>
+        </div>
+      </div>
+    @endforeach
+  </div>
+</div>
+@endsection
 
 @section('scripts')
 <script type="text/javascript">
   $(document).ready(function(){
     $(".clickable-row").click(function() {
-        window.location = $(this).data("href");
+       window.location = $(this).data("href");
     });
   });
 </script>
