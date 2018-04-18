e152fbd6241cf7c70a3dc23967d2dcabebb3a71d
unknown <hacktiegr1989@gmail.com>
2018-04-18 22:32:40 +0700

    add chart but still thinking of best way

---
+++ Laravel/resources/views/testPage.php
@@ -0,0 +1,42 @@
+<?php
+ 
+$dataPoints = array(
+	array("x"=> 10, "y"=> 41),
+	array("x"=> 20, "y"=> 35, "indexLabel"=> "Lowest"),
+	array("x"=> 30, "y"=> 50),
+	array("x"=> 40, "y"=> 45),
+
+);
+	
+?>
+<!DOCTYPE HTML>
+<html>
+<head>  
+<script>
+window.onload = function () {
+ 
+var chart = new CanvasJS.Chart("chartContainer", {
+	animationEnabled: true,
+	exportEnabled: true,
+	theme: "light1", // "light1", "light2", "dark1", "dark2"
+	title:{
+		text: "Simple Column Chart with Index Labels"
+	},
+	data: [{
+		type: "column", //change type to bar, line, area, pie, etc
+		//indexLabel: "{y}", //Shows y value on all Data Points
+		indexLabelFontColor: "#5A5757",
+		indexLabelPlacement: "outside",   
+		dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
+	}]
+});
+chart.render();
+ 
+}
+</script>
+</head>
+<body>
+<div id="chartContainer" style="height: 370px; width: 60%;"></div>
+<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+</body>
+</html>
