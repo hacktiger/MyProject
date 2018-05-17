d01f7b5a235e68c622b2194bf6b38160b6efd8dd
unknown <hacktiegr1989@gmail.com>
2018-05-17 08:30:42 +0700

    seed

--- Laravel/database/seeds/DatabaseSeeder.php
+++ Laravel/database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
 
 
@@ -13,47 +14,83 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        
-        for($i = 0;$i<50;$i++){
+        $date = Carbon::create(2018, 4, 30, 0, 0, 0);
+        //MAKE USERS
+        for($i = 0;$i<500;$i++){
             DB::table('users')->insert([
                 'name' => str_random(10),
                 'email' => str_random(10).'@gmail.com',
                 'password' => bcrypt('secret'),
-                'wallet' => mt_rand (1000,9999)/100,
+                'wallet' => mt_rand (1000,99999)/100,
+                'description' => str_random(100),
+                'created_at' => $date->format('Y-m-d H:i:s'),
 
             ]);
 
         }
-
-
+        // MAKE GAMES
+        for($i = 0; $i<200; $i++){
+            DB::table('games')->insert([
+                'title'=> 'sample game'.$i,
+                'slug'=> 'sample-game'.$i,
+                'description' => 'none',
+                'release' => 1998,
+                'link' => 'none',
+                'upload_by' => 'admin',
+                'price' => mt_rand(1000,6000)/100,
+                'sales' => 0,
+                'created_at' => $date->subDays(rand(1,30))->format('Y-m-d H:i:s'),
+                'updated_at' => $date->addDays(rand(1,10))->format('Y-m-d H:i:s')
+            ]);
+        }
+        // MAKE TAGS
+        for($i = 0; $i<15;$i++ ){
+            DB::table('tags')->insert([
+                'name'=>str_random(20),
+                'created_at' => $date->subDays(rand(1,30))->format('Y-m-d H:i:s'),
+            ]);
+        }
         
 
-        for($i = 0;$i<20;$i++){
-            $date = Carbon::create(2018, 4, 30, 0, 0, 0);
+
+        // sales_log /rating /favorite
+        $games = DB::table('games')->select('title')->get();
+        foreach ($games as $game) {
             DB::table('sales_log')->insert([
-                
-                'game_title'=> 'Counter-Strike : Global Offensive',
-                'user_id'=> mt_rand(1,50),
-                'price' => 40.00,
-                'created_at'  => $date->subDays(rand(1,6))->format('Y-m-d H:i:s')
+                'game_title' => $game->title,
+                'user_id' => mt_rand(1,499),
+                'price' => $game->price,
+                'created_at'  => $date->subDays(rand(1,30))->format('Y-m-d H:i:s'),
+            ]);
+            //games_tags
+            DB::table('games_tags')->insert([
+                'games_title'=>$game_title,
+                'tags_id'=>mt_rand(1,20);
             ]);
 
-            DB::table('sales_log')->insert([
-                
-                'game_title'=> 'StarCraft 2 : Legacy of the Void',
-                'user_id'=> mt_rand(1,50),
-                'price' => 35.00,
-                'created_at'  => $date->subDays(rand(1,6))->format('Y-m-d H:i:s')
+            DB::table('rating')->insert([
+                'game_title' => $game->title,
+                'user_id' => mt_rand(1,150),
+                'rating' => mt_rand(1,2),
             ]);
 
-            DB::table('sales_log')->insert([
-                
-                'game_title'=> 'Nier: Automata',
-                'user_id'=> mt_rand(1,50),
-                'price'=>60.00,
-                'created_at'  => $date->subDays(rand(1,6))->format('Y-m-d H:i:s')
+            DB::table('rating')->insert([
+                'game_title' => $game->title,
+                'user_id' => mt_rand(160,320),
+                'rating' => mt_rand(3,5),
             ]);
 
-        }              
+            DB::table('rating')->insert([
+                'game_title' => $game->title,
+                'user_id' => mt_rand(330,499),
+                'rating' => mt_rand(1,5),
+            ]);
+        }    
+
+        for($i=0; $i<500;$i++){
+            'user_id'=>mt_rand(1,500),
+            'amount'=>mt_rand(1000,3000)/100,
+            'created_at'  => $date->subDays(rand(1,30))->format('Y-m-d H:i:s'),
+        }     
    }
 }
